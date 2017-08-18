4522b5cd1725462d614180e7b22ac485847f3870
xiecheng <xiecheng900424@qq.com>
2017-08-18 09:19:56 +0800

    添加了slider和form控件

---
+++ XCYN.EasyUI/RegisterHandler.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XCYN.EasyUI
+{
+    /// <summary>
+    /// RegisterHandler 的摘要说明
+    /// </summary>
+    public class RegisterHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string name = context.Request["name"];
+            string email = context.Request["email"];
+            string user_name = context.Request["user_name"];
+            context.Response.Write("name:" + name + "_email:" + email + "_user_name:"+ user_name);
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ XCYN.EasyUI/script/33.js
@@ -0,0 +1,22 @@
+﻿
+$(function () {
+    $("#box").form({
+        url: "RegisterHandler.ashx",
+        onSubmit:function(param)
+        {
+            //param.user_name = "xc";
+            return $(this).form("validate");
+        },
+        success:function(data)
+        {
+            alert(data);
+        }
+    });
+
+    //$("#box").form("load", {
+    //    name: "xc",
+    //    email:"cheng@qq.com"
+    //})
+
+    //$("#box").form("load","33_data.json")
+})
