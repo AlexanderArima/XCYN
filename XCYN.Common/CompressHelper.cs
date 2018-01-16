76f70a34173503a739f25647013a7e00aa045d87
亚历山大 <xiecheng900424@qq.com>
2018-01-16 10:50:04 +0800

    添加文件，文件夹，字符串，字符数组的压缩算法

---
+++ XCYN.Common/CompressHelper.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XCYN.Common
+{
+    public class CompressHelper
+    {
+        /// <summary>
+        /// 压缩字节数组
+        /// </summary>
+        /// <param name="str"></param>
+        public static byte[] Compress(byte[] inputBytes)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                using (GZipStream zipStream = new GZipStream(outStream, CompressionMode.Compress, true))
+                {
+                    zipStream.Write(inputBytes, 0, inputBytes.Length);
+                    zipStream.Close(); //很重要，必须关闭，否则无法正确解压
+                    return outStream.ToArray();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 解压缩字节数组
+        /// </summary>
+        /// <param name="str"></param>
+        public static byte[] Decompress(byte[] inputBytes)
+        {
+
+            using (MemoryStream inputStream = new MemoryStream(inputBytes))
+            {
+                using (MemoryStream outStream = new MemoryStream())
+                {
+                    using (GZipStream zipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+                    {
+                        zipStream.CopyTo(outStream);
+                        zipStream.Close();
+                        return outStream.ToArray();
+                    }
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// 压缩字符串
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Compress(string input)
+        {
+            byte[] inputBytes = Encoding.Default.GetBytes(input);
+            byte[] result = Compress(inputBytes);
+            return Convert.ToBase64String(result);
+        }
+
+        /// <summary>
+        /// 解压缩字符串
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Decompress(string input)
+        {
+            byte[] inputBytes = Convert.FromBase64String(input);
+            byte[] depressBytes = Decompress(inputBytes);
+            return Encoding.Default.GetString(depressBytes);
+        }
+
+        /// <summary>
+        /// 压缩目录
+        /// </summary>
+        /// <param name="dir"></param>
+        public static void Compress(DirectoryInfo dir)
+        {
+            foreach (FileInfo fileToCompress in dir.GetFiles())
+            {
+                Compress(fileToCompress);
+            }
+        }
+
+        /// <summary>
+        /// 压缩目录
+        /// </summary>
+        /// <param name="dir"></param>
+        public static void Compress(DirectoryInfo dir, DirectoryInfo dest)
+        {
+            foreach (FileInfo fileToCompress in dir.GetFiles())
+            {
+                Compress(fileToCompress,dest);
+            }
+        }
+
+        /// <summary>
+        /// 解压缩目录
+        /// </summary>
+        /// <param name="dir"></param>
+        public static void Decompress(DirectoryInfo dir)
+        {
+            foreach (FileInfo fileToCompress in dir.GetFiles())
+            {
+                Decompress(fileToCompress);
+            }
+        }
+
+        /// <summary>
+        /// 压缩文件
+        /// </summary>
+        /// <param name="fileToCompress"></param>
+        public static void Compress(FileInfo fileToCompress)
+        {
+            using (FileStream originalFileStream = fileToCompress.OpenRead())
+            {
+                if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                {
+                    using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                        {
+                            originalFileStream.CopyTo(compressionStream);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 压缩文件
+        /// </summary>
+        /// <param name="fileToCompress"></param>
+        public static void Compress(FileInfo fileToCompress, DirectoryInfo dest)
+        {
+            using (FileStream originalFileStream = fileToCompress.OpenRead())
+            {
+                if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                {
+                    using (FileStream compressedFileStream = File.Create(dest.FullName + @"\" + fileToCompress.Name + ".gz"))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                        {
+                            originalFileStream.CopyTo(compressionStream);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 压缩文件
+        /// </summary>
+        /// <param name="fileToCompress"></param>
+        public static void Compress(FileInfo fileToCompress,FileInfo dest)
+        {
+            using (FileStream originalFileStream = fileToCompress.OpenRead())
+            {
+                if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                {
+                    using (FileStream compressedFileStream = File.Create(dest.FullName + ".gz"))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                        {
+                            originalFileStream.CopyTo(compressionStream);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 解压缩文件
+        /// </summary>
+        /// <param name="fileToDecompress"></param>
+        public static void Decompress(FileInfo fileToDecompress)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 解压缩文件
+        /// </summary>
+        /// <param name="fileToDecompress"></param>
+        public static void Decompress(FileInfo fileToDecompress, FileInfo dest)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = dest.FullName;
+                //string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(currentFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ XCYN.Test/CompressTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XCYN.Common;
+using System.IO;
+
+namespace XCYN.Test
+{
+    /// <summary>
+    /// CompressTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class CompressTest
+    {
+        public CompressTest()
+        {
+            //
+            //TODO:  在此处添加构造函数逻辑
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///获取或设置测试上下文，该上下文提供
+        ///有关当前测试运行及其功能的信息。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 附加测试特性
+        //
+        // 编写测试时，可以使用以下附加特性: 
+        //
+        // 在运行类中的第一个测试之前使用 ClassInitialize 运行代码
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // 在类中的所有测试都已运行之后使用 ClassCleanup 运行代码
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 在运行每个测试之前，使用 TestInitialize 来运行代码
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 在每个测试运行完之后，使用 TestCleanup 来运行代码
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Compress()
+        {
+            var num = CompressHelper.Compress("1234567890");
+
+            var result = CompressHelper.Decompress(num);
+
+            Assert.AreEqual("1234567890", result);
+        }
+
+        [TestMethod]
+        public void Compress2()
+        {
+            var str = "1234567890";
+            
+            var num = CompressHelper.Compress(Encoding.UTF8.GetBytes(str));
+
+            var result = CompressHelper.Decompress(num);
+
+            Assert.AreEqual("1234567890", Encoding.UTF8.GetString(result));
+        }
+
+        [TestMethod]
+        public void Compress3()
+        {
+            FileInfo file = new FileInfo(@"D:\锋利的SQL.sql");
+            CompressHelper.Compress(file);
+        }
+        
+        [TestMethod]
+        public void Compress4()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\123");
+            CompressHelper.Compress(dir);
+        }
+
+        [TestMethod]
+        public void Compress5()
+        {
+            FileInfo file = new FileInfo(@"D:\锋利的SQL.sql");
+            FileInfo file_dest = new FileInfo(@"D:\123.sql");
+            CompressHelper.Compress(file, file_dest);
+        }
+
+        [TestMethod]
+        public void Decompress()
+        {
+            FileInfo file = new FileInfo(@"D:\锋利的SQL.sql.gz");
+            CompressHelper.Decompress(file);
+        }
+
+
+        [TestMethod]
+        public void Decompress2()
+        {
+            FileInfo file = new FileInfo(@"D:\123.sql.gz");
+            FileInfo file2 = new FileInfo(@"D:\456.sql");
+            CompressHelper.Decompress(file, file2);
+        }
+
+        [TestMethod]
+        public void Decompress3()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\123\");
+            DirectoryInfo dest = new DirectoryInfo(@"D:\456\");
+            CompressHelper.Compress(dir, dest);
+        }
+    }
+}
