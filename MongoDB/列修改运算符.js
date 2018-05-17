0125dbeb12a12ea218527d9a111456d9a88c951b
亚历山大 <xiecheng900424@qq.com>
2018-05-17 08:54:52 +0800

    添加了MongoDB列修改运算符$inc,$mul,$rename,$set,$setOnInsert

---
+++ MongoDB/列修改运算符.js
@@ -0,0 +1,10 @@
+db.getCollection('inventory').update({"_id":1},{$inc:{qty1:1}}) //自增1
+
+db.getCollection('inventory').update({"_id":1},{$mul:{qty1:2}}) //乘以2
+
+db.getCollection('inventory').update({"_id":1},{$rename:{"qty":"price"}}) //修改列名称
+
+db.getCollection('inventory').update({"_id":1},{$set:{price:1}}) //修改列的值
+db.getCollection('inventory').update({"_id":1},{$set:{'item.name':'xc'}}) //创建之前未存在的列
+
+db.getCollection('inventory').update({"_id":6},{$setOnInsert:{'date':'2018-01-01'}},{upsert:true})
