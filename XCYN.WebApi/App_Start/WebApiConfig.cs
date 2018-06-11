14f96e9abc0c9d31b1605f1a4cd8676acea50e72
AlexanderArima <xiecheng900424@qq.com>
2018-06-11 17:19:59 +0800

    增加了用CORS跨域提交表单的Demo

--- XCYN.WebApi/App_Start/WebApiConfig.cs
+++ XCYN.WebApi/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace XCYN.WebApi
 {
@@ -10,6 +12,7 @@ namespace XCYN.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API 配置和服务
+            EnableCrossSiteRequests(config);
 
             // Web API 路由
             config.MapHttpAttributeRoutes();
@@ -19,6 +22,23 @@ namespace XCYN.WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            //config.Formatters.Clear();
+            //config.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+
+        /// <summary>
+        /// 允许跨域调用
+        /// </summary>
+        /// <param name="config"></param>
+        private static void EnableCrossSiteRequests(HttpConfiguration config)
+        {
+            //对所有的请求来源没有任何限制
+            var cors = new EnableCorsAttribute(
+             origins: "*",
+             headers: "*",
+             methods: "*"
+             );
+            config.EnableCors(cors);
         }
     }
 }
--- XCYN.WebApi/Controllers/UserController.cs
+++ XCYN.WebApi/Controllers/UserController.cs
@@ -17,18 +17,20 @@ namespace XCYN.WebApi.Controllers
         }
 
         // GET: api/User/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/User
-        public IDictionary<string,object> Post(string UserName,string PassWord,string Sex,string Year,string Month,string Day)
+        public IDictionary<string,object> Get(User user)
         {
             Dictionary<string, object> dict = new Dictionary<string, object>();
             dict.Add("stateCode", 1);
-            //var json = "cb(" + JsonConvert.SerializeObject(dict) + ")";
-            //return json;
+            return dict;
+        }
+        
+        // POST: api/User
+        //public IDictionary<string,object> Post(string action,string UserName,string PassWord,
+        //    string Sex,string Year,string Month,string Day)
+        public IDictionary<string,object> Post(User user)
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            dict.Add("stateCode", 1);
             return dict;
         }
 
@@ -41,5 +43,16 @@ namespace XCYN.WebApi.Controllers
         public void Delete(int id)
         {
         }
+    }
+
+    public class User
+    {
+        public string action { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string Sex { get; set; }
+        public string Year { get; set; }
+        public string Month { get; set; }
+        public string Day { get; set; }
     }
 }
