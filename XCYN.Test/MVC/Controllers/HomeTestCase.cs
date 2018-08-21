ea3d1e0217e1f079d2f8da4e471e08ebdcde5039
AlexanderArima <xiecheng900424@qq.com>
2018-08-21 16:01:24 +0800

    将MVC项目的.NET版本改成4.5.2，新建了Controller的测试用例

---
+++ XCYN.Test/MVC/Controllers/HomeTestCase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCYN.MVC.Controllers;
+
+namespace XCYN.Test.MVC.Controllers
+{
+    [TestClass()]
+    public class HomeTestCase
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            HomeController contr = new HomeController();
+            contr.Index();
+            Assert.IsNotNull(contr.ViewBag.Greeting);
+        }
+    }
+}
