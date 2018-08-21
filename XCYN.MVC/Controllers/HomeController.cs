4d351dfb21c6b6691e59c6b8b7483842657db5b9
AlexanderArima <xiecheng900424@qq.com>
2018-08-21 13:44:17 +0800

    添加了页面动态输出，SQL注入，XSS注入，CSRF注入的笔记

--- XCYN.MVC/Controllers/HomeController.cs
+++ XCYN.MVC/Controllers/HomeController.cs
@@ -11,7 +11,30 @@ namespace XCYN.MVC.Controllers
         // GET: Home
         public ViewResult Index()
         {
+            var date = DateTime.Now;
+            if(date.Hour <= 11)
+            {
+                ViewBag.Greeting = "Good Morning";
+            }
+            else if(date.Hour > 11 && date.Hour < 18)
+            {
+                ViewBag.Greeting = "Good Afternoon";
+            }
+            else if(date.Hour < 24)
+            {
+                ViewBag.Greeting = "Good Evening";
+            }
             return View();
+        }
+
+        public RedirectResult Redir()
+        {
+            return Redirect("Index");
+        }
+
+        public HttpUnauthorizedResult UnAuth()
+        {
+            return new HttpUnauthorizedResult();
         }
     }
 }
