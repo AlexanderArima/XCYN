c54d14594c7ceb66082d8780f682855b7f3b4e83
AlexanderArima <xiecheng900424@qq.com>
2018-09-19 15:01:13 +0800

    添加授权过滤器和异常过滤器

---
+++ XCYN.Filter/App_Start/CustomExceptionFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XCYN.Filter.App_Start
+{
+    /// <summary>
+    /// 自定义异常处理类
+    /// </summary>
+    public class CustomExceptionAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            var values = filterContext.RouteData.Values;
+            var controllerName = values["controller"].ToString();
+            var actionName = values["action"].ToString();
+            filterContext.Result = new RedirectResult("~/Content/ErrorPage.html");
+            filterContext.ExceptionHandled = true;  //将它设置为true，系统就不会出现“黄色屏幕”了
+        }
+    }
+}
---
+++ XCYN.Filter/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace XCYN.Filter.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username,string password,string returnUrl)
+        {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                return Redirect(returnUrl ?? Url.Action("Index", "Home"));
+            }
+            else
+            {
+                ModelState.AddModelError("", "错误的用户名或密码");
+                return View();
+            }
+        }
+    }
+}
