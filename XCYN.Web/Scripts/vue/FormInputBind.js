20a5a97fdae804f63a856231ca67a90ebf9dd3a6
AlexanderArima <xiecheng900424@qq.com>
2018-09-26 16:04:38 +0800

    添加Vue的表单输入绑定

---
+++ XCYN.Web/Scripts/vue/FormInputBind.js
@@ -0,0 +1,16 @@
+﻿var app = new Vue({
+    el: "#app",
+    data: {
+        UserName: "",
+        Password: "",
+        ButtonName: "启动",
+        Birthday: "",
+        ThemeColor: "",
+        HiddenValue: "1778",
+        Price: 0,
+        CountryImage: "../../Image/image1.jpeg",
+        Sex: "unknown",
+        Fruit: [],
+        Mark:"",
+    }
+});
