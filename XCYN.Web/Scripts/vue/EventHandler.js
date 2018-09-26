d7b097e92d45def97bc5efe0d0e3f8b0f28c7566
AlexanderArima <xiecheng900424@qq.com>
2018-09-26 11:08:47 +0800

    添加Vue事件绑定的Demo

---
+++ XCYN.Web/Scripts/vue/EventHandler.js
@@ -0,0 +1,17 @@
+﻿var app = new Vue({
+    el: "#app",
+    data: {
+
+    },
+    methods: {
+        Submit: function (event) {
+            console.log("提交 表单");
+        },
+        FormClick: function (event) {
+            console.log("点击表单");
+        },
+        Valid: function () {
+            console.log("验证");
+        }
+    }
+});
