003b53246fc4048062f4f3e97f62ccc056ffc3f4
AlexanderArima <xiecheng900424@qq.com>
2018-09-27 09:26:44 +0800

    使用.NET Core的中Microsoft.Extensions.Configuration加载Json，XML，环境变量，程序内部的配置项

---
+++ XCYN.Core.Print/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace XCYN.Core.Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("username","Cheng")
+            };
+            //加载程序中的配置项除了必须要添加的Microsoft.Extensions.Configuration，不需要其他的依赖项
+            //加载Json文件需要添加Microsoft.Extensions.Configuration.Json这个依赖项
+            //加载Xml文件需要添加Microsoft.Extensions.Configuration.Xml这个依赖项
+            //加载环境变量需要添加Microsoft.Extensions.Configuration.EnvironmentVariables这个依赖项
+            IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory)
+                                             //.AddJsonFile("appsetting.json", optional: true, reloadOnChange: true)
+                                             //.AddXmlFile("appsetting.xml",optional:true,reloadOnChange:true)
+                                             //.AddEnvironmentVariables("Path")
+                                             .AddInMemoryCollection(list)
+                                             .Build();
+
+            //相同的键，只取最后一个
+            while (true)
+            {
+                var info = configuration["username"];
+
+                Console.WriteLine(info);
+
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
