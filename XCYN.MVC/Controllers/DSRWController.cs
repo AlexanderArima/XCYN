4d8fc33b47b1dbcf782842b3c4f36aeb722ec0d6
XieCheng <xiecheng900424@qq.com>
2019-10-23 15:01:46 +0800

    添加了定时任务Web框架的Job删除，暂停，恢复功能

--- XCYN.MVC/Controllers/DSRWController.cs
+++ XCYN.MVC/Controllers/DSRWController.cs
@@ -1,5 +1,6 @@
 ﻿using Quartz;
 using Quartz.Impl;
+using Quartz.Impl.Matchers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,7 @@ namespace XCYN.MVC.Controllers
         // GET: DSRW
         public ActionResult Index()
         {
+            //schedule
             var meta = scheduler.GetMetaData();
             var name = scheduler.SchedulerName;
             var id = scheduler.SchedulerInstanceId;
@@ -41,17 +43,37 @@ namespace XCYN.MVC.Controllers
                 JobNumbers = meta.NumberOfJobsExecuted.ToString(),
             };
             ViewData["scheObj"] = obj;
+            //Job
             var list = scheduler.GetJobGroupNames();
             var jobs = scheduler.GetJobKeys(Quartz.Impl.Matchers.GroupMatcher<JobKey>.GroupContains(""));
             List<JobObj> list_job = new List<JobObj>();
             foreach (var item in jobs)
             {
+                var detail = scheduler.GetJobDetail(item);
                 list_job.Add(new JobObj {
                     Name = item.Name,
-                    GroupName = item.Group
+                    GroupName = item.Group,
+                    Description = detail.Description,
+                    IsDurable = detail.Durable,
+                    Type = detail.JobType.FullName
                 });
             }
             ViewData["jobObj"] = list_job;
+            //触发器(Trigger)
+            var triggers = scheduler.GetTriggerKeys(GroupMatcher<TriggerKey>.GroupContains(""));
+            List<TriggerObj> list_trigger = new List<TriggerObj>();
+            foreach (var item in triggers)
+            {
+                var detail = scheduler.GetTrigger(item);
+                list_trigger.Add(new TriggerObj() {
+                    Name = item.Name,
+                    GroupName = item.Group,
+                    Description = detail.Description,
+                    StartTime = detail.StartTimeUtc.ToString("yyyy-MM-dd HH:mm:ss"),
+                    EndTime = detail.EndTimeUtc.HasValue? detail.EndTimeUtc.Value.ToString("yyyy-MM-dd HH:mm:ss") : "",
+                    LastTime = detail.FinalFireTimeUtc.HasValue ? detail.FinalFireTimeUtc.Value.ToString("yyyy-MM-dd HH:mm:ss") : "",
+                });
+            }
             return View();
         }
 
@@ -94,6 +116,97 @@ namespace XCYN.MVC.Controllers
             });
         }
 
+        /// <summary>
+        /// 删除Job
+        /// </summary>
+        /// <param name="jobName"></param>
+        /// <param name="groupName"></param>
+        /// <returns></returns>
+        public JsonResult DeleteJob(string jobName,string groupName)
+        {
+            try
+            {
+                var flag = scheduler.DeleteJob(new JobKey(jobName, groupName));
+                if(flag == true)
+                {
+                    return Json(new
+                    {
+                        code = "1",
+                        msg = ""
+                    });
+                }
+                else
+                {
+                    return Json(new
+                    {
+                        code = "0",
+                        msg = "删除失败"
+                    });
+                }
+            }
+            catch(Exception ex)
+            {
+                return Json(new
+                {
+                    code = "999999",
+                    msg = ex.Message
+                });
+            }
+        }
+
+        /// <summary>
+        /// 暂停Job
+        /// </summary>
+        /// <param name="jobName"></param>
+        /// <param name="groupName"></param>
+        /// <returns></returns>
+        public JsonResult PauseJob(string jobName, string groupName)
+        {
+            try
+            {
+                scheduler.PauseJob(new JobKey(jobName, groupName));
+                return Json(new
+                {
+                    code = "1",
+                    msg = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    code = "999999",
+                    msg = ex.Message
+                });
+            }
+        }
+
+        /// <summary>
+        /// 暂停Job
+        /// </summary>
+        /// <param name="jobName"></param>
+        /// <param name="groupName"></param>
+        /// <returns></returns>
+        public JsonResult ResumeJob(string jobName, string groupName)
+        {
+            try
+            {
+                scheduler.ResumeJob(new JobKey(jobName, groupName));
+                return Json(new
+                {
+                    code = "1",
+                    msg = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    code = "999999",
+                    msg = ex.Message
+                });
+            }
+        }
     }
 
     public class ScheduleObject
@@ -123,5 +236,53 @@ namespace XCYN.MVC.Controllers
         /// Group名称
         /// </summary>
         public string GroupName { get; set; }
+
+        /// <summary>
+        /// Job描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 是否持久化
+        /// </summary>
+        public bool IsDurable { get; set; }
+
+        /// <summary>
+        /// 类型名称(带命名空间)
+        /// </summary>
+        public string Type { get; set; }
+    }
+
+    public class TriggerObj
+    {      
+        /// <summary>
+        /// Job名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Group名称
+        /// </summary>
+        public string GroupName { get; set; }
+
+        /// <summary>
+        /// Job描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 起始时间
+        /// </summary>
+        public string StartTime { get; set; }
+
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public string EndTime { get; set; }
+
+        /// <summary>
+        /// 最近一次执行时间
+        /// </summary>
+        public string LastTime { get; set; }
     }
 }
