53e4e290d7bd597cc55dae28bd7bc4ea9bcaac0a
XieCheng <xiecheng900424@qq.com>
2019-10-28 17:13:57 +0800

    添加了显示，编辑，删除Calendar的方法，编辑Job和Trigger的方法

--- XCYN.MVC/Controllers/DSRWController.cs
+++ XCYN.MVC/Controllers/DSRWController.cs
@@ -1,5 +1,7 @@
 ﻿using Quartz;
+using Quartz.Collection;
 using Quartz.Impl;
+using Quartz.Impl.Calendar;
 using Quartz.Impl.Matchers;
 using System;
 using System.Collections.Generic;
@@ -88,6 +90,25 @@ namespace XCYN.MVC.Controllers
                 
             }
             ViewData["triggerObj"] = list_trigger;
+            //排除日历(Calendar)
+            List<CalendarObj> list_calendar = new List<CalendarObj>();
+            var calendars = scheduler.GetCalendarNames();
+            for (int i = 0; i < calendars.Count; i++)
+            {
+                var item = scheduler.GetCalendar(calendars[i]) as DailyCalendar;
+                var calendarName = calendars[i];
+                var startTime = item.GetTimeRangeStartingTimeUtc(DateTimeOffset.Now).ToString("yyyy-MM-dd HH:mm:ss");
+                var endTime = item.GetTimeRangeEndingTimeUtc(DateTimeOffset.Now).ToString("yyyy-MM-dd HH:mm:ss");
+                CalendarObj detail = new CalendarObj()
+                {
+                    calendarName = calendarName,
+                    startTime = startTime,
+                    endTime = endTime,
+                    calendarType = "DailyCalendar",
+                };
+                list_calendar.Add(detail);
+            }
+            ViewData["calendarObj"] = list_calendar;
             return View();
         }
 
@@ -125,6 +146,39 @@ namespace XCYN.MVC.Controllers
                 });
             }
             return Json(new {
+                code = "1",
+                msg = ""
+            });
+        }
+
+        /// <summary>
+        /// 编辑Job
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult EditJob(string jobName, string groupName, string className, string namespaceName, string description)
+        {
+            try
+            {
+                //Load(命名空间).GetType(命名空间.类名)
+                var type = Assembly.Load(namespaceName).GetType(namespaceName + "." + className);
+           
+                var job = JobBuilder.Create(type).StoreDurably(true)
+                                                  .WithDescription(description)
+                                                  .WithIdentity(jobName, groupName)
+                                                  .Build();
+                scheduler.AddJob(job, true);
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    code = "999999",
+                    msg = ex.Message
+                });
+            }
+            return Json(new
+            {
                 code = "1",
                 msg = ""
             });
@@ -253,6 +307,58 @@ namespace XCYN.MVC.Controllers
             catch(Exception ex)
             {
                 return Json(new {
+                    code = "999999",
+                    msg = ex.Message
+                });
+            }
+        }
+
+        /// <summary>
+        /// 编辑触发器
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult EditTrigger(DSRWControlViewModel_AddTrigger model)
+        {
+            try
+            {
+                var jobDetail = scheduler.GetJobDetail(new JobKey(model.jobName, model.jobGroupName));
+                var trigger = TriggerBuilder.Create()
+                                                       .StartNow()
+                                                       .ForJob(model.jobName, model.jobGroupName)
+                                                       .WithIdentity(model.triggerName, model.triggerGroupName)
+                                                       .WithCronSchedule(model.cornExpression)
+                                                       .WithDescription(model.description)
+                                                       .Build();
+                var list_trigger = scheduler.GetTriggerKeys(GroupMatcher<TriggerKey>.AnyGroup());
+                var hash = new Quartz.Collection.HashSet<ITrigger>();
+                foreach (var item in list_trigger)
+                {
+                    if(item.Name == model.triggerName && item.Group == model.triggerGroupName)
+                    {
+                        var tri = TriggerBuilder.Create()
+                                                       .StartNow()
+                                                       .ForJob(model.jobName, model.jobGroupName)
+                                                       .WithIdentity(model.triggerName, model.triggerGroupName)
+                                                       .WithCronSchedule(model.cornExpression)
+                                                       .WithDescription(model.description)
+                                                       .Build();
+                        //var tri =  scheduler.GetTrigger(new TriggerKey(model.triggerName, model.triggerGroupName));
+                        hash.Add(tri);
+                    }
+                }
+               
+                scheduler.ScheduleJob(jobDetail, hash, true);
+                
+                return Json(new
+                {
+                    code = "1",
+                    msg = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
                     code = "999999",
                     msg = ex.Message
                 });
@@ -393,6 +499,156 @@ namespace XCYN.MVC.Controllers
                 });
             }
         }
+
+        /// <summary>
+        /// 添加Calendar
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult AddCalendar(DSRWControlViewModel_AddCalendar model)
+        {
+            if(model.calendarType == "DailyCalendar")
+            {
+                var list_startTime = model.startTime.Split(':');
+                var list_endTime = model.endTime.Split(':');
+                if (list_startTime.Length > 1 && list_endTime.Length > 1)
+                {
+                    var cale = new DailyCalendar(
+                       string.Format("{0}:{1}:{2}", list_startTime.ElementAt(0),
+                       list_startTime.ElementAt(1),
+                       list_startTime.ElementAt(2)),
+                       string.Format("{0}:{1}:{2}", list_endTime.ElementAt(0),
+                       list_endTime.ElementAt(1),
+                       list_endTime.ElementAt(2))
+                       );
+                    cale.Description = model.description;
+                    scheduler.AddCalendar(model.calendarName, cale, true, true);
+                    var trigger = scheduler.GetTrigger(new TriggerKey(model.triggerName, model.triggerGroup));
+                    if(trigger != null)
+                    {
+                        //如果触发器存在则，修改触发器，调用修改Calendar方法
+                        var newTrigger = trigger.GetTriggerBuilder().ModifiedByCalendar(model.calendarName).Build();
+                        //更新Schedule
+                        scheduler.RescheduleJob(new TriggerKey(model.triggerName, model.triggerGroup), newTrigger);
+                    }
+                    return Json(new {
+                        code = "1",
+                        msg = ""
+                    });
+                }
+                else
+                {
+                    return Json(new {
+                        code = "999999",
+                        msg = "请输入起始时间和结束时间"
+                    });
+                }
+            }
+            else
+            {
+                return Json(new {
+                    code = "101",
+                    msg = "目前只支持DaliyCalendar模式"
+                });
+            }
+        }
+
+
+        /// <summary>
+        /// 修改Calendar
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult EditCalendar(DSRWControlViewModel_AddCalendar model)
+        {
+            if (model.calendarType == "DailyCalendar")
+            {
+                var list_startTime = model.startTime.Split(':');
+                var list_endTime = model.endTime.Split(':');
+                if (list_startTime.Length > 1 && list_endTime.Length > 1)
+                {
+                    var cale = new DailyCalendar(
+                       string.Format("{0}:{1}:{2}", list_startTime.ElementAt(0),
+                       list_startTime.ElementAt(1),
+                       list_startTime.ElementAt(2)),
+                       string.Format("{0}:{1}:{2}", list_endTime.ElementAt(0),
+                       list_endTime.ElementAt(1),
+                       list_endTime.ElementAt(2))
+                       );
+                    cale.Description = model.description;
+                    scheduler.AddCalendar(model.calendarName, cale, true, true);
+                    var trigger = scheduler.GetTrigger(new TriggerKey(model.triggerName, model.triggerGroup));
+                    if (trigger != null)
+                    {
+                        //如果触发器存在则，修改触发器，调用修改Calendar方法
+                        var newTrigger = trigger.GetTriggerBuilder().ModifiedByCalendar(model.calendarName).Build();
+                        //更新Schedule
+                        scheduler.RescheduleJob(new TriggerKey(model.triggerName, model.triggerGroup), newTrigger);
+                    }
+                    return Json(new
+                    {
+                        code = "1",
+                        msg = ""
+                    });
+                }
+                else
+                {
+                    return Json(new
+                    {
+                        code = "999999",
+                        msg = "请输入起始时间和结束时间"
+                    });
+                }
+            }
+            else
+            {
+                return Json(new
+                {
+                    code = "101",
+                    msg = "目前只支持DaliyCalendar模式"
+                });
+            }
+        }
+
+        /// <summary>
+        /// 删除Calendar
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult DeleteCalendar(string calendarName)
+        {
+            //var trigger = scheduler.GetTrigger(new TriggerKey("", ""));
+            var triggerKey = scheduler.GetTriggerKeys(GroupMatcher<TriggerKey>.AnyGroup());
+            foreach (var item in triggerKey)
+            {
+                var trigger = scheduler.GetTrigger(item);
+                if(calendarName == trigger.CalendarName)
+                {
+                    var newTrigger = trigger.GetTriggerBuilder().ModifiedByCalendar(null).Build();
+                    scheduler.RescheduleJob(item, newTrigger);
+                    var flag = scheduler.DeleteCalendar(calendarName);
+                    if (flag == true)
+                    {
+                        return Json(new
+                        {
+                            code = "1",
+                            msg = ""
+                        });
+                    }
+                    else
+                    {
+                        return Json(new
+                        {
+                            code = "101",
+                            msg = "删除失败"
+                        });
+                    }
+                }
+            }
+            return Json(new
+            {
+                code = "101",
+                msg = "删除失败"
+            });
+        }
+
     }
 
     public class ScheduleObject
@@ -486,4 +742,39 @@ namespace XCYN.MVC.Controllers
         /// </summary>
         public string State { get; set; }
     }
+
+    public class CalendarObj
+    {
+        /// <summary>
+        /// 日历名称
+        /// </summary>
+        public string calendarName { get; set; }
+
+        /// <summary>
+        /// 日历类型
+        /// </summary>
+        public string calendarType { get; set; }
+
+        /// <summary>
+        /// 起始时间
+        /// </summary>
+        public string startTime { get; set; }
+
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public string endTime { get; set; }
+
+        /// <summary>
+        /// 触发器的组名
+        /// </summary>
+        public string triggerGroup { get; set; }
+
+        /// <summary>
+        /// 触发器的名称
+        /// </summary>
+        public string triggerName { get; set; }
+    }
+
+   
 }
--- XCYN.MVC/ViewModel/DSRWControlViewModel.cs
+++ XCYN.MVC/ViewModel/DSRWControlViewModel.cs
@@ -41,4 +41,46 @@ namespace XCYN.MVC.ViewModel
         /// </summary>
         public string jobGroupName { get; set; }
     }
+    public class DSRWControlViewModel_AddCalendar
+    {
+        /// <summary>
+        /// 日历名称
+        /// </summary>
+        public string calendarName { get; set; }
+
+        /// <summary>
+        /// 日历类型
+        /// </summary>
+        public string calendarType { get; set; }
+
+        /// <summary>
+        /// 日历的值
+        /// </summary>
+        public string calendarValue { get; set; }
+
+        /// <summary>
+        /// 起始时间
+        /// </summary>
+        public string startTime { get; set; }
+
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public string endTime { get; set; }
+
+        /// <summary>
+        /// 触发器的组名
+        /// </summary>
+        public string triggerGroup { get; set; }
+
+        /// <summary>
+        /// 触发器的名称
+        /// </summary>
+        public string triggerName { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string description { get; set; }
+    }
 }
