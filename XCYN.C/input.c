d2a969282ad984a4cef955f607005983ca6db36c
XieCheng <xiecheng900424@qq.com>
2020-05-26 08:33:13 +0800

    添加了输入字符串类型的gets(),getchar(),getch(),getche()中文输出方法wprintf()

--- XCYN.C/input.c
+++ XCYN.C/input.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<Windows.h>
+#include <Windows.h>
+#include <conio.h>
 
+//int Fun6() {
 int main() {
 	int a, b = 0;
 	//&称为取地址符，也就是获取变量在内存中的地址
@@ -10,8 +12,26 @@ int main() {
 	//printf("a + b = %d\n", a + b);
 
 	//打印地址
-	char c = ' ';
-	printf("a& = %p b& = %p c& = %p\n", &a, &b, &c);
+	//char c = ' ';
+	//printf("a& = %p b& = %p c& = %p\n", &a, &b, &c);
+
+	//getchar()是scanf()的替代品
+	//char d = getchar();
+	//printf("d = %c\n", d);
+
+	//_getche()方法和getchar()的区别在于没有缓冲区，不用按Enter键就能输出在控制台上
+	//char e = _getche();
+	//printf("e = %c\n", e);
+
+	//_getch()方法和_getche()方法的区别在于，它不会回显用户的输入字符，在有些场合需要这种特性，比如输入密码就得防止别人偷窥
+	//char f = _getch();
+	//printf("f = %c\n", f);
+
+	//gets()方法和scanf()的区别在于gets()只能输入字符串类型并且可以打印空格，scanf()可以输入多种类型，但不能打印空格
+	char g[20];
+	gets(g);
+	//scanf("%s", g);
+	printf("g = %s\n", g);
 	system("pause");
 	return 0;
 }
--- XCYN.C/str.c
+++ XCYN.C/str.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
 
 int Fun3() {
+//int main()	{
 	int a = 'a';
 	int b = 98;
 	printf("a = %d %c\n", a, a);
@@ -13,7 +16,15 @@ int Fun3() {
 	printf("d = %s\n", d);
 
 	printf("I\'m Jim.\t He say \"Hurry Up\" \n");
-	printf("hello");
+	
+	char e = '一';	//打印的是乱码，因为char类型的窄字符是用ASCII编码
+	printf("%c \n", e);
+	//想要输出中文首先要定义一个wchar_t类型，这个类型可以输出宽字符，并在前面加L
+	wchar_t f = L'一';
+	//并设置本地语言环境为中文
+	setlocale(LC_ALL, "zh-CN");
+	//输出得使用wprintf函数，并在前面加L，宽字符对应的占位符为%lc
+	wprintf(L"%lc \n", f);
 	system("pause");
 	return 0;
 }
