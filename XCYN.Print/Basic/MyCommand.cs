3b5bf88cf3e7f091144316b093e313e6a46a88b7
XieCheng <xiecheng900424@qq.com>
2021-07-02 15:50:44 +0800

    添加了内插字符串字面量的Demo

--- XCYN.Print/Basic/MyCommand.cs
+++ XCYN.Print/Basic/MyCommand.cs
@@ -16,5 +16,15 @@ namespace XCYN.Print.Basic
                 throw new Exception("");
             }
         }
+
+        /// <summary>
+        /// 使用内插字符串的自变量.
+        /// </summary>
+        public static void Fun02()
+        {
+            var id = "202107020001";
+            var name = "张三";
+            Console.WriteLine($"The id is {id}, The name is {name}");
+        }
     }
 }
