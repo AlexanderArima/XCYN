676e0c2055f57698aafa6d76a53c73984e7ab248
XieCheng <xiecheng900424@qq.com>
2023-04-18 10:18:17 +0800

    添加异步执行的Demo

--- XCYN.Winform/AsyncDemo/MainForm.Designer.cs
+++ XCYN.Winform/AsyncDemo/MainForm.Designer.cs
@@ -32,6 +32,9 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,22 +44,25 @@
             this.textBox1.Location = new System.Drawing.Point(3, 17);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(325, 178);
+            this.textBox1.Size = new System.Drawing.Size(437, 260);
             this.textBox1.TabIndex = 0;
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(331, 198);
+            this.groupBox1.Size = new System.Drawing.Size(443, 280);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "日志区";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(187, 216);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(15, 298);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -66,7 +72,8 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(268, 216);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(96, 298);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -74,11 +81,47 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(177, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "执行异步2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(258, 298);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "执行多个异步";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(352, 298);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "执行多个异步2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(355, 251);
+            this.ClientSize = new System.Drawing.Size(467, 329);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
@@ -96,5 +139,8 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
--- XCYN.Winform/AsyncDemo/MainForm.cs
+++ XCYN.Winform/AsyncDemo/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -21,9 +22,9 @@ namespace XCYN.Winform.AsyncDemo
         private void button1_Click(object sender, EventArgs e)
         {
             // 同步调用，UI界面会卡住
-            this.textBox1.Text = "查询开始\r\n" + this.textBox1.Text;
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询开始\r\n" + this.textBox1.Text;
             Query();
-            this.textBox1.Text =  "查询结束\r\n" + this.textBox1.Text;
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询结束\r\n" + this.textBox1.Text;
         }
 
         private List<string> Query()
@@ -36,17 +37,130 @@ namespace XCYN.Winform.AsyncDemo
         {
             // 异步调用，调用了有async的异步方法，为了让它实现异步调用，需使用await关键字，并且方法自身
             // 添加async关键字修饰
-            this.textBox1.Text = "查询开始\r\n" + this.textBox1.Text;
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询开始\r\n" + this.textBox1.Text;
             var result = await QueryAsync();
-            this.textBox1.Text = "查询结束\r\n" + this.textBox1.Text;
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询结束\r\n" + this.textBox1.Text;
+
+            // ===========================等待3s后继续=======================
+            await Task.Delay(3000);
+
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，添加开始\r\n" + this.textBox1.Text;
+            await Insert();
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，添加结束\r\n" + this.textBox1.Text;
         }
 
         private async Task<List<string>> QueryAsync()
         {
             // 异步调用，Task.Delay返回Task，那么就可以使用await关键字调用，这样就是调用异步方法了
             // 调用异步方法的代价就是，需要将方法自身添加async关键字，并在方法的后缀添加Async标记
+            Debug.WriteLine(DateTime.Now.ToString("HH:mm:ss") + "，开始查询");
+
             await Task.Delay(5000);
-            return null;
+
+            Debug.WriteLine(DateTime.Now.ToString("HH:mm:ss") + "，结束查询");
+            return new List<string>()
+            {
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+            };
+        }
+
+        private async Task<int> QueryAsync2()
+        {
+            // 异步调用，Task.Delay返回Task，那么就可以使用await关键字调用，这样就是调用异步方法了
+            // 调用异步方法的代价就是，需要将方法自身添加async关键字，并在方法的后缀添加Async标记
+            Debug.WriteLine(DateTime.Now.ToString("HH:mm:ss") + "，开始查询");
+
+            await Task.Delay(3000);
+
+            Debug.WriteLine(DateTime.Now.ToString("HH:mm:ss") + "，结束查询");
+            return 10;
+        }
+
+        private async Task Insert()
+        {
+            await Task.Delay(1000);
+        }
+
+        /// <summary>
+        /// 返回了空的Task对象
+        /// </summary>
+        /// <returns></returns>
+        private Task GetNullTask()
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 返回一个空的带泛型的Task对象
+        /// </summary>
+        /// <returns></returns>
+        private Task<string> GetStringTask()
+        {
+            return Task.FromResult("Love");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 方法不使用async修饰，调用的异步方法也不使用await，而是在异步方法后继续调用GetAwaiter().GetResult()，但这种方法会引起线程的阻塞，不推荐使用
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询开始\r\n" + this.textBox1.Text;
+            var result = QueryAsync().GetAwaiter().GetResult();
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询结束\r\n" + this.textBox1.Text;
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询开始\r\n" + this.textBox1.Text;
+            List<Task> list_task = new List<Task>();
+            var t1 = QueryAsync();
+            var t2 = QueryAsync2();
+            list_task.Add(t1);
+            list_task.Add(t2);
+
+            // Task.WhenAll的使用场景是一个任务被拆成多个子任务，需要等待所有任务完成后才会继续执行
+            Task result = Task.WhenAll(list_task);
+
+            await result;
+
+            // 获取各个任务的返回值，作为下一步任务的基础
+            var result_t1 = t1.Result;
+            var result_t2 = t2.Result;
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询结束\r\n" + this.textBox1.Text;
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询开始\r\n" + this.textBox1.Text;
+            List<Task> list_task = new List<Task>();
+            var t1 = QueryAsync();
+            var t2 = QueryAsync2();
+            list_task.Add(t1);
+            list_task.Add(t2);
+
+            // Task.WhenAny的使用场景是一个任务被拆成多个子任务，只要某一个任务执行完成，就会继续执行
+            Task result = Task.WhenAny(list_task);
+
+            await result;
+            
+            if(t1.Status == TaskStatus.RanToCompletion)
+            {
+                var result_t1 = t1.Result;
+            }
+
+            if(t2.Status == TaskStatus.RanToCompletion)
+            {
+                var result_t2 = t2.Result;
+            }
+
+            this.textBox1.Text = DateTime.Now.ToString("HH:mm:ss") + "，查询结束\r\n" + this.textBox1.Text;
         }
     }
 }
