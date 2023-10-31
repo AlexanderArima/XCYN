e9b92d06d3faec06845d2103106e653eb9fde93a
Arima <xiecheng900424@qq.com>
2023-10-31 09:49:39 +0800

    上传一个为了熟悉ASP.NET MVC的登录功能，目前实现了前端的提交，后端的数据接收，还未实现数据库的交互

---
+++ XCYN.MVC/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XCYN.MVC.Models;
+
+namespace XCYN.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 验证用户是否有效.
+        /// </summary>
+        /// <returns></returns>
+        public string ValidUser(string username, string password)
+        {
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ReceiveObject obj = new ReceiveObject();
+                obj.code = "999999";
+                obj.msg = "用户名、密码不能为空";
+                return JsonConvert.SerializeObject(obj);
+            }
+
+            if(this.Valid(username, password))
+            {
+                ReceiveObject obj = new ReceiveObject();
+                obj.code = "0";
+                obj.msg = "成功";
+                return JsonConvert.SerializeObject(obj);
+            }
+            else
+            {
+                ReceiveObject obj = new ReceiveObject();
+                obj.code = "999999";
+                obj.msg = "用户名、密码错误";
+                return JsonConvert.SerializeObject(obj);
+            }
+        }
+
+        private bool Valid(string username, string password)
+        {
+            if(username.Equals("admin") && password.Equals("123456"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ XCYN.MVC/Models/ReceiveObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XCYN.MVC.Models
+{
+    public class ReceiveObject
+    {
+        public string code { get; set; }
+
+        public string msg { get; set; }
+
+        public object data { get; set; }
+    }
+}
