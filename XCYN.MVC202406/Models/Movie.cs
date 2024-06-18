67f94693ba4c4c19a1e54ddcc0d3779f49c5a2e5
Arima <xiecheng900424@qq.com>
2024-06-18 17:28:58 +0800

    添加ASP.NET MVC的入门教程

---
+++ XCYN.MVC202406/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XCYN.MVC202406.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
