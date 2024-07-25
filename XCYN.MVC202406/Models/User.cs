0263a245725b825099624dbecbe1878c37678664
Arima <xiecheng900424@qq.com>
2024-07-25 16:33:57 +0800

    添加从Control传递数据到View，Razor语法的Demo

---
+++ XCYN.MVC202406/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XCYN.MVC202406.Models
+{
+    /// <summary>
+    /// 用户类.
+    /// </summary>
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
