b867919b019fe5168ea0d55628b85c0b61ee9c16
XC <917563264@qq.com>
2024-07-29 11:07:30 +0800

    添加OCR识别框架PaddleSharp的Demo

---
+++ XCYN.WinformX64/OCR/PictureOCRForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace XCYN.WinformX64.OCR
+{
+    partial class PictureOCRForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 246);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(286, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "开始识别";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 246);
+            this.textBox1.TabIndex = 3;
+            // 
+            // PictureOCRForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 341);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PictureOCRForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片文字识别器";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ XCYN.WinformX64/OCR/PictureOCRForm.cs
@@ -0,0 +1,81 @@
+﻿using OpenCvSharp;
+using Sdcb.PaddleInference;
+using Sdcb.PaddleOCR;
+using Sdcb.PaddleOCR.Models;
+using Sdcb.PaddleOCR.Models.Local;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XCYN.WinformX64.OCR
+{
+    public partial class PictureOCRForm : Form
+    {
+        /// <summary>
+        /// 选中图片的路径
+        /// </summary>
+        private string _pictureFullPath;
+
+        public PictureOCRForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 选择图片.
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            dialog.FilterIndex = 1;
+            dialog.Multiselect = false;
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                // 选中图片
+                _pictureFullPath = dialog.FileName;
+                Image image = Image.FromFile(_pictureFullPath);
+                pictureBox1.Image = image;
+            }
+            else
+            {
+                MessageBox.Show("您本次没有选择任何图片", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /// <summary>
+        /// 开始识别.
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FullOcrModel model = LocalFullModels.ChineseV3;
+            using (PaddleOcrAll all = new PaddleOcrAll(model, PaddleDevice.Mkldnn())
+            {
+                AllowRotateDetection = true,    // 允许有角度的文字
+                Enable180Classification = false    // 允许旋转角度180度的文字
+            })
+            {
+                using(Mat src = Cv2.ImRead(this._pictureFullPath))
+                {
+                    // 返回OCR的结果
+                    PaddleOcrResult result = all.Run(src);
+                    if (string.IsNullOrEmpty(result.Text))
+                    {
+                        MessageBox.Show("未能识别出文字", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        this.textBox1.Text = result.Text;
+                    }
+                    else
+                    {
+                        this.textBox1.Text = result.Text;
+                    }
+                }
+            };
+        }
+    }
+}
