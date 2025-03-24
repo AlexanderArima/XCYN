a2d2601ade28aa21f06b5144fedadb3c0434553a
XC <xiecheng900424@qq.com>
2025-03-24 13:52:07 +0800

    添加Vue3.0组件基础的例子

---
+++ XCYN.VueApp/src/components/1.组件基础.vue
@@ -0,0 +1,50 @@
+<template>
+  <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        components: {
+        },
+        directives: {
+        },
+        filters: {
+        },
+        props: {
+        },
+        data() {
+          return {
+            count: 0
+            }
+        },
+        computed: {
+        },
+        watch: {
+        },
+        beforeCreate() {
+        },
+        created() {
+        },
+        beforeMount() {
+        },
+        mounted() {
+        },
+        updated() {
+        },
+        activated() {
+        },
+        deactivated() {
+        },
+        beforeDestroy() {
+        },
+        destroyed() {
+        },
+        methods: {
+        },
+    });
+</script>
+
+<style>
+</style>
