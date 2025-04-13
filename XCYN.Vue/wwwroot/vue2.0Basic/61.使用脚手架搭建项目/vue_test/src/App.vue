ce87fa1baaa525fce691dc89faee1aef4de40e4a
XC <xiecheng900424@qq.com>
2025-04-13 15:35:01 +0800

    再次添加脚手架项目

---
+++ XCYN.Vue/wwwroot/vue2.0Basic/61.使用脚手架搭建项目/vue_test/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <school></school>
+      <student></student>
+  </div>
+</template>
+
+<script>
+
+  import School from "./components/School.vue";
+  import Student from "./components/Student.vue"
+
+  export default {
+      name:"App",
+      components:{
+          School,
+          Student
+      }
+  }
+
+</script>
