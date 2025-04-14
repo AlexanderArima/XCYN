76e8cb97ddeb783a0f8efe41925c5838f605dbbf
XC <xiecheng900424@qq.com>
2025-04-14 15:57:13 +0800

    添加ref属性，props配置项的Demo

---
+++ XCYN.Vue/wwwroot/vue2.0Basic/61.使用脚手架搭建项目/vue_test/60_搭建Vue脚手架_src/components/Student.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>学生姓名：{{studentName}}</h2>
+        <br/>
+        <h2>学生年龄：{{studentAge}}</h2>
+        <br/> 
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Student",
+        data(){
+            return {
+                studentName: "张三",
+                studentAge: 18
+            }
+        }
+    }
+</script>
---
+++ XCYN.Vue/wwwroot/vue2.0Basic/61.使用脚手架搭建项目/vue_test/65_ref属性_src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h2 ref="hello">你好啊</h2>
+      <school ref="sch"></school>
+      <button @click="ShowMsg">点我显示ref</button>
+  </div>
+</template>
+
+<script>
+
+  import School from "./components/School.vue";
+
+  export default {
+      name:"App",
+      components:{
+          School
+      },
+      methods:{
+        ShowMsg(){
+          // 显示id为hello的Dom元素
+          console.log(this.$refs.hello)
+
+          // 显示sch组件的实例对象，后期在需要获取组件的值的时候会用到
+          console.log(this.$refs.sch)
+        }
+      }
+  }
+
+</script>
